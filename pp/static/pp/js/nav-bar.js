6b17524132e0425269eb7f6d5cdceacdfac24f82
theuppercaseguy <saadan06@gmail.com>
2022-09-01 16:27:00 -0700

    added javascript to side buttons

--- pp/static/pp/js/nav-bar.js
+++ pp/static/pp/js/nav-bar.js
@@ -7,6 +7,7 @@ const navToggle = document.querySelector(".mobile-nav-toggle");
 navToggle.addEventListener("click",()=>{
     const visibility = primaryNav.getAttribute("data-visible");
     console.log(visibility);
+    
     if(visibility === "false"){
         primaryNav.setAttribute('data-visible',true);
         navToggle.setAttribute('aria-expanded',true); 
