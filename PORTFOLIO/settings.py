e21f83072e498c537869ca9d97e56a7d36b5ef72
theuppercaseguy <saadan06@gmail.com>
2022-11-11 17:39:45 -0800

    responsiveness imorived

--- PORTFOLIO/settings.py
+++ PORTFOLIO/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3vvuy1+!m9_2ym8*#(2n9hv&2o-ik$pk9!qbvrvd(*cz@w*jx%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-DEBUG = True
+DEBUG = False
+# DEBUG = True
 
 ALLOWED_HOSTS = ['*',]
 
