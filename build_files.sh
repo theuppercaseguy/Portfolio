ef77ec0a5635f68301d69eda96ce1a78c80f469a
theuppercaseguy <saadan06@gmail.com>
2023-07-20 14:43:46 +0500

    chnaging files

--- build_files.sh
+++ build_files.sh
@@ -2,7 +2,7 @@ echo "BUILD START"
 
 
 python3.9 -m pip install -r requirements.txt
-python3.9 -m manage.py collectstatic --noinout --clear    
+python3.9 -m manage.py collectstatic --noinput --clear    
 
 
 
