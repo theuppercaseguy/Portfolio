b00303ca38a045b01b5eb2b8ac3954fccf9b4d73
theuppercaseguy <saadan06@gmail.com>
2023-07-20 16:05:50 +0500

    versal working, database added. everything works fine

--- PORTFOLIO/settings.py
+++ PORTFOLIO/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles","static")
 
