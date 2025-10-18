d8525a14fe2b834e1a47adbb2da7b7bdaa2ed056
Saad Khan <saadan06@gmail.com>
2025-10-18 19:13:45 +0500

    asdf

--- PORTFOLIO/settings.py
+++ PORTFOLIO/settings.py
@@ -135,7 +135,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'pp' / 'staticfiles',
+    BASE_DIR / 'staticfiles',
     ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
