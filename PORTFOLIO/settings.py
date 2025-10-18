5223e572834ac79337bab6452f94371aa3d4d579
Saad Khan <saadan06@gmail.com>
2025-10-18 18:13:45 +0500

    updates

--- PORTFOLIO/settings.py
+++ PORTFOLIO/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-3vvuy1+!m9_2ym8*#(2n9hv&2o-ik$pk9!qbvrvd(*cz@w*jx%
 # DEBUG = False
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','.vercel.app','.now.sh','*']
+ALLOWED_HOSTS = ['*']
 
 # --- Email configuration ---
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
